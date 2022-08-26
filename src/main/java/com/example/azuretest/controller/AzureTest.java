1ff44d262cfbe8c1a31fe00be3ff56d31a40bf2d
Baeist <lars18n2@stud.kea.dk>
2022-08-26 10:04:52 +0200

    add thymeleaf dependency

--- src/main/java/com/example/azuretest/controller/AzureTest.java
+++ src/main/java/com/example/azuretest/controller/AzureTest.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AzureTest {
 
     @GetMapping("/")
     public String sayHello(){
 
-        return "Hello";
+        return "index";
     }
 }
