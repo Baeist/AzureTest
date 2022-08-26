0aff147d30c0440891fb9e867da2afcf2d6a96b2
Baeist <lars18n2@stud.kea.dk>
2022-08-26 09:03:29 +0200

    initial

---
+++ src/main/java/com/example/azuretest/controller/AzureTest.java
@@ -0,0 +1,15 @@
+package com.example.azuretest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureTest {
+
+    @GetMapping("/")
+    public String sayHello(){
+
+        return "Hello";
+    }
+}
