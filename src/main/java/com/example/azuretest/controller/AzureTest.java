7badfaeb59690f803ab8ed956637c35fcf6af463
Baeist <lars18n2@stud.kea.dk>
2022-09-05 20:05:36 +0200

    update url to test azure

--- src/main/java/com/example/azuretest/controller/AzureTest.java
+++ src/main/java/com/example/azuretest/controller/AzureTest.java
@@ -1,14 +1,19 @@
 package com.example.azuretest.controller;
 
+import com.example.azuretest.utility.ConnectionManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.sql.Connection;
 
 
 @Controller
 public class AzureTest {
 
-    @GetMapping("/")
+    @GetMapping("/hi")
     public String sayHello(){
+
+        Connection connection = ConnectionManager.getConnection();
 
         return "index";
     }
